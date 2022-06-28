3a7fd4d75230c06dd0e416845b03af4999797f62
Syerne <shubhamyerne1994@gmail.com>
2022-06-28 20:19:20 +0530

    Company wage

--- CompanyEmployeeWage.java
+++ CompanyEmployeeWage.java
@@ -3,13 +3,19 @@ public class CompanyEmployeeWage {
     static final int FULL_TIME_WORKING_HRS = 8, PART_TIME_WORKING_HRS = 4,
                     WORKING_DAY_FOR_MONTH = 20, TOTAL_WORKING_HRS = 100;
 
+    static int monthlyWage = 0;
+    static final int workingHrsPerMonth = 0;
+    static final int workingHrPerDay = 0;;
     public static void main(String[] args) {
         display();
         CompanyEmployeeWage attendenceCheck = new CompanyEmployeeWage();
-        int workingHrsPerDay = attendenceCheck.switchCase();
-        attendenceCheck.dailyWage(workingHrsPerDay);
-        int monthlyWage = attendenceCheck.monthlyWage();
-        System.out.println("Employee Monthly Wage : " + monthlyWage);
+        attendenceCheck.dailyWage(8);
+        attendenceCheck.dailyWage(8);
+
+        int monthlyWageReliance = attendenceCheck.monthlyWage(10, 25, 200);
+        System.out.println("Employee Monthly Wage for Company  : " + monthlyWageReliance);
+        int monthlyWageDmart = attendenceCheck.monthlyWage(20, 30, 200);
+        System.out.println("Employee Monthly Wage for Company  : " + monthlyWageDmart);
     }
 
     public static void display() {
@@ -33,9 +39,9 @@ public class CompanyEmployeeWage {
         }
         return workingHrsPerDay;
     }
-    public int switchCase() {
+    public int switchCase( int workingHrPerDay) {
 
-        int workingHrPerDay;
+
         int rand = (int) (Math.random() * 3);
 
         switch (rand) {
@@ -56,10 +62,9 @@ public class CompanyEmployeeWage {
         }
         return workingHrPerDay;
     }
-    public int monthlyWage() {
+    public int monthlyWage(int wagePerHrs, int workingHrPerDay, int workingHrsPerMonth) {
         CompanyEmployeeWage monthlyWageCalculator = new CompanyEmployeeWage();
 
-        int monthlyWage = 0, workingHrsPerMonth = 0;
 
         for (int i = 1; i <= WORKING_DAY_FOR_MONTH; i++) {
             System.out.println("Day-" + i);
