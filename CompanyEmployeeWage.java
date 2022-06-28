4da6ae4d82623f07a005aff640b167e6c9482a96
Syerne <shubhamyerne1994@gmail.com>
2022-06-28 18:54:18 +0530

    Welcome Message

---
+++ CompanyEmployeeWage.java
@@ -0,0 +1,8 @@
+public class CompanyEmployeeWage {
+    public static void main(String[] args) {
+        display();
+    }
+    public static void display(){
+        System.out.println("Welcome to Employee Wage Calculation");
+    }
+}
