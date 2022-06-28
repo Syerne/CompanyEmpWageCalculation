0365245e92981eaae952ad4768ba9b050d63f353
Syerne <shubhamyerne1994@gmail.com>
2022-06-28 19:16:21 +0530

    Daily Employee Wage

--- CompanyEmployeeWage.java
+++ CompanyEmployeeWage.java
@@ -1,22 +1,41 @@
 public class CompanyEmployeeWage {
     static final int EMP_IS_PRESENT = 1;
+    static final int FULL_TIME_WORKING_HRS = 8;
+
     public static void main(String[] args) {
         display();
         CompanyEmployeeWage attendenceCheck = new CompanyEmployeeWage();
-        attendenceCheck.attendanceCheck();
+        int workingHrsPerDay = attendanceCheck();
+        attendenceCheck.dailyWage(workingHrsPerDay);
     }
 
     public static void display() {
         System.out.println("Welcome to Employee Wage Calculation");
     }
 
-    public void attendanceCheck() {
+    public static int attendanceCheck() {
 
         int empRandomCheck = (int) (Math.random() * 2);
 
-        if (empRandomCheck == EMP_IS_PRESENT)
+        int workingHrsPerDay;
+        if (empRandomCheck == EMP_IS_PRESENT) {
             System.out.println("Employee is Present");
-        else
+            workingHrsPerDay = FULL_TIME_WORKING_HRS;
+        } else {
             System.out.println("Employee is Absent");
+            workingHrsPerDay = 0;
+        }
+        return workingHrsPerDay;
     }
+
+    public int dailyWage(int workingHrPerDay) {
+
+        int WAGE_PER_HRS = 20;
+
+        int dailyWage = WAGE_PER_HRS * workingHrPerDay;
+        System.out.println("Daily wage: " + dailyWage);
+
+        return dailyWage;
+    }
+
 }
